0c0f1e66fddf8a1629804ffaa476b87b6d6384c4
Grzegorz Hilgier <g.hilgier@gmail.com>
2019-04-02 20:43:14 +0200

    commit

---
+++ WebApp/Controllers/ClientController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public ClientController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Client
+        [HttpGet]
+        public IEnumerable<Client> GetClient()
+        {
+            return _context.Client;
+        }
+
+        // GET: api/Client/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetClient([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var client = await _context.Client.FindAsync(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(client);
+        }
+
+        // PUT: api/Client/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClient([FromRoute] int id, [FromBody] Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != client.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Client
+        [HttpPost]
+        public async Task<IActionResult> PostClient([FromBody] Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Client.Add(client);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetClient", new { id = client.Id }, client);
+        }
+
+        // DELETE: api/Client/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var client = await _context.Client.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            _context.Client.Remove(client);
+            await _context.SaveChangesAsync();
+
+            return Ok(client);
+        }
+
+        private bool ClientExists(int id)
+        {
+            return _context.Client.Any(e => e.Id == id);
+        }
+    }
+}
