6198985fe5abff0b00f94dc3f87deb05c0285599
Grzegorz Hilgier <g.hilgier@gmail.com>
2019-04-06 19:08:56 +0200

    add password options

---
+++ WebApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public AuthenticationController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        //POST : /api/Authentication/Register
+        public async Task<object> PostApplicationUser(ApplicationUserModel model)
+        {
+            var applicationUser = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                CompanyName = model.CompanyName
+            };
+
+            try 
+	        {
+                var result = await _userManager.CreateAsync(applicationUser, model.Password);
+                return Ok(result);
+	        }
+	        catch (global::System.Exception ex)
+	        {
+
+		        throw ex;
+	        }
+        }
+    }
+}
